b1d7a53d6436aeb471315dfc68c836ba34435908
SW1pr0g <poryadin_alex@mail.ru>
2022-12-12 22:26:45 +0300

    Add basic view materials, search

---
+++ demoexam_example/Materials.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using demoexam_example.classes;
+using MySql.Data.MySqlClient;
+
+namespace demoexam_example
+{
+    public partial class Materials : Window
+    {
+        public Materials()
+        {
+            InitializeComponent();
+            ShowData("SELECT Image, (SELECT train.MaterialType.Title FROM train.MaterialType WHERE train.Material.MaterialTypeID = train.MaterialType.ID ) AS MaterialType, Title, CountInStock, MinCount FROM train.Material;");
+        }
+
+        private void ShowData(string sql)
+        {
+            using (MySqlConnection conn = new MySqlConnection(classes.Variables.ConnStr))
+            {
+                try
+                {
+                    conn.Open();
+
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    {
+                        List <classes.Material> material = new List<classes.Material>();
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string ImageSource = reader.GetString(0);
+                                if (ImageSource == String.Empty)
+                                    ImageSource = "images/picture.png";
+                                else
+                                    ImageSource = "images" + ImageSource;
+                                 
+                                material.Add(new classes.Material(){ Image = ImageSource, MaterialType = reader.GetString(1),Title = reader.GetString(2), 
+                                    CountInStock = reader.GetInt32(3), MinCount = reader.GetInt32(4) });
+                            }
+                            MaterialsList.ItemsSource = material;
+                        }
+                    }
+                }
+                catch (MySqlException)
+                {
+                    MessageBox.Show("Ошибка подключения к БД!");
+                }
+            }   
+        }
+
+        private void SearchBox_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            ShowData($"SELECT Image, (SELECT train.MaterialType.Title FROM train.MaterialType WHERE train.Material.MaterialTypeID = train.MaterialType.ID ) AS MaterialType, Title, CountInStock, MinCount FROM train.Material WHERE Material.Title LIKE '%{SearchBox.Text}%';");
+        }
+    }
+}
---
+++ demoexam_example/classes/Material.cs
@@ -0,0 +1,11 @@
+﻿namespace demoexam_example.classes
+{
+    public class Material
+    {
+        public string Image { get; set; }
+        public string MaterialType { get; set; }
+        public string Title { get; set; }
+        public int CountInStock { get; set; }
+        public int MinCount { get; set; }
+    }
+}
