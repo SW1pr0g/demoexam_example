ac7de003e7f2e348a32b74900ff842d3b7135246
SW1pr0g <poryadin_alex@mail.ru>
2022-12-16 00:23:07 +0300

    Fix branch

--- demoexam_example/classes/Variables.cs
+++ demoexam_example/classes/Variables.cs
@@ -11,6 +11,5 @@ namespace demoexam_example.classes
         public static string ConnStr = "server=localhost;user=root;pwd=;database=train;";
         public static string ExitMsg = "Вы действительно хотите выйти из приложения?";
         public static string ExitTitle = "Выход из приложения";
-        public static bool AuthClosed = false;
     }
 }
